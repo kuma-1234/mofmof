246faa4d419c0f6fdecf7eb1684c703dbe3adab2
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-20 16:11:05 +0900

    空のレコードができないように設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ApplicationRecord
   has_many :stations, dependent: :destroy
-  accepts_nested_attributes_for :stations
+  accepts_nested_attributes_for :stations, reject_if: :all_blank
 end
 
